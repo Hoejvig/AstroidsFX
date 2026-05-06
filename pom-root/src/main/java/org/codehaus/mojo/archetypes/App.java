f048b16ad2188e1395142a20a935ecd35b613dc6
Victor Brønnum Højvig <Victorbonnum@gmail.com>
2026-05-06 10:56:07 +0200

    Project startet

---
+++ pom-root/src/main/java/org/codehaus/mojo/archetypes/App.java
@@ -0,0 +1,10 @@
+package org.codehaus.mojo.archetypes;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
